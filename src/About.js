9d5639765e00f76bd11cd03792657669d81b384f
Jyotsna-1010 <150793698+Jyotsna-1010@users.noreply.github.com>
2024-03-07 12:35:36 +0530

    Update About.js

--- src/About.js
+++ src/About.js
@@ -5,7 +5,8 @@ const About = () => {
   const { myName } = useProductContext();
 
   const data = {
-    name: "Demo E-commerce",
+    name: "Our mission is to empower both customers and sellers by providing a seamless and enjoyable shopping experience. We strive to curate a diverse selection of [products] while maintaining the highest standards of quality and customer service.
+      Thank you for choosing [Your E-commerce Website Name] – we can't wait to embark on this journey with you!",
   };
 
   return (
