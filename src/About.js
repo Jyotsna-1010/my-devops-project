d408e6bf9604f8da41d414630fbe9a6fc6f883da
Jyotsna-1010 <150793698+Jyotsna-1010@users.noreply.github.com>
2024-03-07 13:02:01 +0530

    Update About.js

--- src/About.js
+++ src/About.js
@@ -16,7 +16,7 @@ const About = () => {
     </div>
     </wrapper>
   );
-}
+};
 
   const data = {
     name: "Our mission is to empower both customers and sellers by providing a seamless and enjoyable shopping experience. We strive to curate a diverse selection of [products] while maintaining the highest standards of quality and customer service.
