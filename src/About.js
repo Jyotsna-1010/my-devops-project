fedced7c7618a36f8f4b8d12c01627fc85081374
Jyotsna-1010 <150793698+Jyotsna-1010@users.noreply.github.com>
2024-03-11 15:23:34 +0530

    Update About.js

--- src/About.js
+++ src/About.js
@@ -5,8 +5,7 @@ const About = () => {
   const { myName } = useProductContext();
 
   const data = {
-    name: "Our mission is to empower both customers and sellers by providing a seamless and enjoyable shopping experience. We strive to curate a diverse selection of [products] while maintaining the highest standards of quality and customer service.
-      Thank you for choosing [Your E-commerce Website Name] – we can't wait to embark on this journey with you!",
+    name: "Discover a world of style and convenience right at your fingertips. At ShopSphere, we curate the latest trends and timeless classics to bring you an unparalleled shopping experience",
   };
 
   return (
