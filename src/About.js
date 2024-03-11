c39864393c9d4bef44c8b8da81c87b8aca63f705
Jyotsna-1010 <150793698+Jyotsna-1010@users.noreply.github.com>
2024-03-11 15:08:49 +0530

    Update About.js

--- src/About.js
+++ src/About.js
@@ -4,20 +4,6 @@ import { useProductContext } from "./context/productcontex";
 const About = () => {
   const { myName } = useProductContext();
 
-  return (
-    <Wrapper>
-      <div className="container">
-        <div className="grid grid-two-column">
-          <div className="about-data">
-            <p className="intro-data"><b>MISSION</b></p>
-            <h1> {name} </h1>
-          </div>
-        </div>
-      </div>
-    </wrapper>
-  );
-};
-
   const data = {
     name: "Our mission is to empower both customers and sellers by providing a seamless and enjoyable shopping experience. We strive to curate a diverse selection of [products] while maintaining the highest standards of quality and customer service.
       Thank you for choosing [Your E-commerce Website Name] – we can't wait to embark on this journey with you!",
@@ -31,4 +17,4 @@ const About = () => {
   );
 };
 
-export default About;
+export default About;
